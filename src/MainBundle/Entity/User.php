4aa1f16c9e12ced3bcb359c6a9eb1c47309a271d
slagme <slawomir.gmerek@gmail.com>
2017-05-10 12:01:29 +0200

    Added User Entity with Task relationship

--- src/MainBundle/Entity/User.php
+++ src/MainBundle/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace MainBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 /**
@@ -19,6 +20,46 @@ class User extends BaseUser
     public function __construct()
     {
         parent::__construct();
+        $this->tasks = new ArrayCollection();
+    }
 
+    /**
+     * @ORM\OneToMany(targetEntity="task", mappedBy="user")
+     *
+     */
+
+    private $tasks;
+
+    /**
+     * Add tasks
+     *
+     * @param \MainBundle\Entity\task $tasks
+     * @return User
+     */
+    public function addTask(\MainBundle\Entity\task $tasks)
+    {
+        $this->tasks[] = $tasks;
+
+        return $this;
+    }
+
+    /**
+     * Remove tasks
+     *
+     * @param \MainBundle\Entity\task $tasks
+     */
+    public function removeTask(\MainBundle\Entity\task $tasks)
+    {
+        $this->tasks->removeElement($tasks);
+    }
+
+    /**
+     * Get tasks
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getTasks()
+    {
+        return $this->tasks;
     }
 }
