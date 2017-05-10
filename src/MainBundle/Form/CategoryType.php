9f21ae4148be795a14b81caf037245eee6a9567b
slagme <slawomir.gmerek@gmail.com>
2017-05-10 12:42:46 +0200

    Crud for Category made

---
+++ src/MainBundle/Form/CategoryType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('users');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MainBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_category';
+    }
+
+
+}
