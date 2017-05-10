984266cb6d177c9a7fc5bdf210a2faee25ade4a4
slagme <slawomir.gmerek@gmail.com>
2017-05-10 12:38:54 +0200

    Controller and Repository set

---
+++ src/MainBundle/Controller/TaskController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use MainBundle\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Task controller.
+ *
+ * @Route("task")
+ */
+class TaskController extends Controller
+{
+    /**
+     * Lists all task entities.
+     *
+     * @Route("/", name="task_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $tasks = $em->getRepository('MainBundle:Task')->findAll();
+
+        return $this->render('task/index.html.twig', array(
+            'tasks' => $tasks,
+        ));
+    }
+
+    /**
+     * Finds and displays a task entity.
+     *
+     * @Route("/{id}", name="task_show")
+     * @Method("GET")
+     */
+    public function showAction(Task $task)
+    {
+
+        return $this->render('task/show.html.twig', array(
+            'task' => $task,
+        ));
+    }
+}
