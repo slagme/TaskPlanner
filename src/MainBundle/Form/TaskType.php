cd5ba4e829bc646e3a262a3afda54886be060c09
slagme <slawomir.gmerek@gmail.com>
2017-05-10 16:41:50 +0200

    Task Type form created

---
+++ src/MainBundle/Form/TaskType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')
+            ->add('description')
+            ->add('deadline');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MainBundle\Entity\Task'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_task';
+    }
+
+
+}
