c21ba32e63b7d4fa3e8f773895e229b29816f813
slagme <slawomir.gmerek@gmail.com>
2017-05-11 10:54:44 +0200

    Changed

--- src/MainBundle/Form/CategoryType.php
+++ src/MainBundle/Form/CategoryType.php
@@ -14,6 +14,7 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name')->add('users');
+
     }
     
     /**
